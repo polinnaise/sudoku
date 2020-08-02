10e089adabf8d16c744e7188c5f7f7c3f56757ec
stranger <stranger@strangers-MacBook-Pro.local>
2020-08-02 18:26:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,227 @@
+<template>
+	<div
+		id="app"
+		class="sky"
+	>
+  <div class="stars">
+		<Menu
+			v-if='start'
+			@reset='start = false'
+		/>
+		<Sudoku
+			v-if='start'
+			:diff='diff'
+			:showDetails='showDetails'
+			:currentGame='currentGame'
+		/>
+		<div
+			v-else
+			class="back"
+		>
+			<h1>Sudoku</h1>
+      <div>
+			<button
+				@click='currentGame = false; start=true'
+				class="neon"
+			>
+      New Game
+      </button>
+			<button
+				class="con"
+				v-if='hasCurrentGame'
+				@click='currentGame = true; start = true'
+			>
+			Continue
+			</button>
+      </div>  
+		</div>
+    </div>
+	</div>
+</template>
+
+<script>
+import Sudoku from "./components/Sudoku.vue";
+import Menu from "./components/Menu.vue";
+
+export default {
+	name: "App",
+	data() {
+		return {
+			diff: 4,
+			start: false,
+			showDetails: true,
+			currentGame: false,
+			hasCurrentGame: false,
+		};
+	},
+	components: {
+		Sudoku,
+		Menu,
+	},
+	created() {
+		if (localStorage.currentGame !== undefined) {
+			this.hasCurrentGame = true;
+		}
+	},
+};
+</script>
+
+<style>
+@font-face {
+  font-family: 'Neuropol';
+  src: url("./font/neuropol.ttf") format("truetype");
+}
+html, body {
+	margin: 0;
+  background-color: #0a2342;
+}
+#app, .back, .timer {
+	display: flex;
+  flex-direction: column;
+	justify-content: center;
+	align-items: center;
+  font-family: 'Neuropol';
+  letter-spacing: 3px;
+  font-size: 19px;
+  color: white;
+}
+.sky {
+  position:absolute;
+  width:100%;
+  height: 100%;
+  background: #4B79A1;
+  background: -webkit-linear-gradient(to top, #0a2342, black);
+  background: linear-gradient(to top, #0a2342, black);
+  background: -olinear-gradient(to top, #0a2342, black); 
+}
+.stars {
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  width:100%;
+  height:100%;
+  display:block;
+  position:absolute;
+  background:url(http://www.script-tutorials.com/demos/360/images/stars.png) repeat top center;
+  z-index:0;
+}
+h1 {
+  margin: 10% 0 0;
+  font-weight: 300;
+  font-size:7em;
+  color: rgba(120, 0, 50, .5);
+  animation: text-flicker 2s linear forwards;
+}
+.neon, .con {
+  width: 200px;
+  height: 50px;
+  padding: 10px 10px;
+  border-radius: 8px;
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 20%;
+  text-shadow: unset;
+}
+.neon:hover, .con:hover {
+  animation: border-flicker 2s linear forwards;
+  text-shadow: 0 0 5px #00E0FF;
+}
+button, button.continue  {
+  all: unset;
+  background: linear-gradient(var(--background-colour));
+  text-align: center;
+  position: relative;
+  border-radius: 8px;
+  border: var(--border-width) solid var(--border-colour);
+  transform: rotateX(15deg);
+  animation: rotate ease-out 6s infinite alternate;
+  border: 2px solid #00E0FF;
+  text-shadow: 0 0 5px #00E0FF;
+  color: #00E0FF;
+  padding: 15px 15px;
+  cursor: pointer;
+}
+@keyframes border-flicker {
+  10% {
+    border: 3px solid rgb(0, 180, 230);
+    box-shadow: 0 0 15px -1px rgb(0, 180, 230), 
+    0 0 12px -1px rgb(0, 180, 230) inset;
+  }
+  15% {
+    border: 3px solid rgb(0, 180, 230);
+    box-shadow: 0 0 15px -1px rgb(0, 180, 230), 
+    0 0 12px -1px rgb(0, 180, 230) inset;
+  }
+}
+@keyframes rotate {
+  0% {
+    transform: rotateY(-40deg);
+  }
+  100% {
+    transform: rotateY(40deg);
+  }
+}
+@keyframes text-flicker {
+  2% { 
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  3% {
+    color: rgba(120, 0, 50, .5);
+    text-shadow: none;
+  }
+  6% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  9% {
+    color: rgba(120, 0, 50, .5);
+    text-shadow: none;
+  }
+  11% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  14% {
+    color: rgba(120, 0, 50, .5);
+    text-shadow: none;
+  }
+  18% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  32% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  33% {
+    color: rgba(120, 0, 50, .5);
+    text-shadow: none;
+  }
+  37% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  39% {
+    color: rgba(120, 0, 50, .5);
+    text-shadow: none;
+  }
+  43% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  46% {
+    color: rgba(120, 0, 50, .5);
+    text-shadow: none;
+  }
+  47% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+  100% {
+    color: rgb(230, 0, 120);
+    text-shadow: 0 0 15px rgb(230, 0, 120);
+  }
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+		<div class="menu">
+			<div
+				@click='$emit("reset")'
+			>
+				<span>Home</span>
+			</div>
+	</div>
+</template>
+
+<style scoped>
+.menu {
+  color: white;
+  font-size: 20px;
+  margin-top: 2%;
+  margin-left: 3%;
+  cursor: pointer;
+  width: 0;
+  height: 0;
+  border-top: 30px solid transparent;
+  border-right: 80px solid rgb(25,141,195,0.5);
+  border-bottom: 30px solid transparent;
+}
+.menu:hover {
+  color: rgb(230, 0, 120);
+  text-shadow: 0 0 15px rgb(230, 0, 120);
+}
+</style>
